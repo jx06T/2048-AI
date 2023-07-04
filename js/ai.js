64adc8c2ad79d68de91bb36b9cb7cdb402fcef7e
jx06T <50313tjx06@gmail.com>
2023-07-05 00:13:47 +0800

    勉強

--- js/ai.js
+++ js/ai.js
@@ -1,15 +1,14 @@
 class GameAI {
     constructor() {
-        this.depth = 8;
         this.DirectionScore = { 0: 0, 1: 0, 2: 0, 3: 0 }
         this.next = Math.floor(Math.random() * 4)
         this.NowD = -1
+        this.EdgeX = [0, 0, 0, 0, 1, 1, 2, 2, 3, 3, 3, 3]
+        this.EdgeY = [1, 0, 2, 3, 0, 3, 0, 3, 0, 1, 2, 3]
     }
     GetNextStep() {
-        // let next = Math.floor(Math.random() * 4)
         let next = this.next
         this.next = null
-        // console.log(next)
         return next
     }
     GridToGrid(grid) {
@@ -21,31 +20,31 @@ class GameAI {
         }
         return NewGrid
     }
+
+
     calculation(NowGrid) {
-        let StartTime = Date.now()
+        console.log(NowGrid)
+        // let StartTime = Date.now()
         this.DirectionScore = { 0: 0, 1: 0, 2: 0, 3: 0 }
         for (let i = 0; i < 4; i++) {
             this.NowD = i
-            let grid = this.move(i, Array.from(NowGrid))
-            if (grid[0] == null) {
-                this.DirectionScore[i] = -10000
+            let grid = this.move(i, recursiveClone(NowGrid))
+            if (grid[0] == null) {//不能移動
+                this.DirectionScore[i] = -10000000
                 continue
             }
-            this.DirectionScore[i] += grid[0]
-            this.Jcomputer(grid[1], 0)
+            this.DirectionScore[i] += grid[1] * 2
+            this.DirectionScore[i] += this.Jcomputer(grid[0], 2)
         }
+        console.log(this.DirectionScore)
         this.next = this.BestD(this.DirectionScore)
-        // console.log(this.next, this.DirectionScore)
         // console.log((Date.now() - StartTime) / 1000)
     }
     move(d, grid) {
-        let score = 0
-        let old = Array.from(grid)
+        let score = 0;
+        let old = recursiveClone(grid)
         let V = jx06_GameManager.getVector(d)
-        if (V.y != 0) {
-            grid = this.ChangeGrid(grid)
-        }
-
+        if (V.y != 0) grid = this.ChangeGrid(grid)
         for (let i = 0; i < grid.length; i++) {
             grid[i] = grid[i].filter((num) => num !== 0);
             let jj = V.x + V.y < 0 ? 0 : grid[i].length - 1
@@ -63,13 +62,9 @@ class GameAI {
                 grid[i].splice(2 + (V.x + V.y) * -2, 0, 0);
             }
         }
-        if (V.y != 0) {
-            grid = this.ChangeGrid(grid)
-        }
-        if (JSON.stringify(old) == JSON.stringify(grid)) {
-            score = null
-        }
-        return [score, grid]
+        if (V.y != 0) grid = this.ChangeGrid(grid)
+        if (JSON.stringify(old) == JSON.stringify(grid)) grid = null
+        return [grid, score]
     }
     ChangeGrid(grid) {
         let NewGrid = [[], [], [], []]
@@ -80,36 +75,57 @@ class GameAI {
         }
         return NewGrid
     }
+
     Jcomputer(grid, steps) {
-        if (steps > 1) {
-            return
+        let grades = 0
+        let count = 0
+        if (steps < 1) {
+            return 0
         }
         for (let x = 0; x < grid.length; x++) {
             for (let y = 0; y < grid.length; y++) {
                 if (grid[y][x] == 0) {
+                    count++
                     grid[y][x] = 2
-                    this.Jplayer(grid, steps, 5)
+                    grades += 0.8 * this.Jplayer(grid, steps)
                     grid[y][x] = 4
-                    this.Jplayer(grid, steps, 1)
+                    grades += 0.2 * this.Jplayer(grid, steps)
                     grid[y][x] = 0
                 }
             }
         }
+        return grades / count
     }
-    Jplayer(NowGrid, steps, type) {
-        steps++
+    Jplayer(NowGrid, steps) {
+        steps--
+        let grades = 0
         for (let i = 0; i < 4; i++) {
-            grid = this.move(i, Array.from(NowGrid))
-            this.DirectionScore[this.NowD] += grid[0] * type
-            this.Jcomputer(grid[1], steps)
+            grid = this.move(i, recursiveClone(NowGrid))
+            // grades += grid[1]*0.1
+            if (grid[0] == null) {
+                return grades
+            }
+            grades += this.heuristics(grid[0])
+            grades += this.Jcomputer(grid[0], steps, grades)
         }
+        return grades
     }
     heuristics(grid) {
+        let grades = 0
         for (let x = 0; x < grid.length; x++) {
             for (let y = 0; y < grid.length; y++) {
-
+                if (grid[y][x] == 0) {
+                    grades += 10
+                }
             }
         }
+        for (let i = 0; i < 12; i++) {
+            if (grid[this.EdgeY[i]][this.EdgeX[i]]>64) {
+                grades += 50
+            }
+
+        }
+        return grades
     }
     BestD(AllD) {
         let max = -Infinity;
@@ -125,6 +141,11 @@ class GameAI {
     }
 }
 
+function recursiveClone(val) {
+    return Array.isArray(val) ? Array.from(val, recursiveClone) : val;
+}
+
+
 function create_previousState() {
     grid = [[],
     [],
@@ -137,7 +158,6 @@ function create_previousState() {
     let NewGrid = [[], [], [], []]
     for (let x = 0; x < grid.length; x++) {
         for (let y = 0; y < grid.length; y++) {
-            // console.log(grid.cells[x][y] != null? gridcells[x][y].value :0)
             aCell = grid[x][y]
             if (aCell == 0 || aCell == undefined) {
                 NewGrid[y].push(null)
--- js/main.js
+++ js/main.js
@@ -1,5 +1,5 @@
 let AI = new GameAI()
-let EXECUTION_STEP_INTERVAL = 300
+let EXECUTION_STEP_INTERVAL = 70
 let isAI_OPEN = false
 const AIinput = document.getElementById("AIinput")
 AIinput.addEventListener("change", () => {
@@ -9,13 +9,17 @@ const AIisOpen = document.getElementById("AIisOpen")
 AIisOpen.addEventListener("change", () => {
     isAI_OPEN = AIisOpen.checked
 })
+const OneStep = document.getElementById('OneStep')
+OneStep.addEventListener("click", () => {
+    temp_EXECUTION_STEP_INTERVAL = -1
+})
 let grid
 let LastTimestamp = 0
 let temp_EXECUTION_STEP_INTERVAL = EXECUTION_STEP_INTERVAL
 function step(timestamp) {
-    if ((jx06_GameManager.keepPlaying ||!jx06_GameManager.over)&&timestamp - LastTimestamp > temp_EXECUTION_STEP_INTERVAL && isAI_OPEN) {
+    if (temp_EXECUTION_STEP_INTERVAL == -1 || (jx06_GameManager.keepPlaying || !jx06_GameManager.won) && !jx06_GameManager.over && timestamp - LastTimestamp > temp_EXECUTION_STEP_INTERVAL && isAI_OPEN) {
         next = AI.GetNextStep()
-        if(next == null){
+        if (next == null) {
             temp_EXECUTION_STEP_INTERVAL = 5
             return
         }
